30bd207bfe60dc38827a0ebe84460dd7afa0432f
callmesiva <sgnathan10@gmail.com>
2024-04-27 12:42:42 +0530

    config changed

--- src/fireBase/fbConfig.js
+++ src/fireBase/fbConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBXKGzEowDj3Zjoow4IxniWxEpjUjYJGfM",
-  authDomain: "ecommerce-20e59.firebaseapp.com",
-  projectId: "ecommerce-20e59",
-  storageBucket: "ecommerce-20e59.appspot.com",
-  messagingSenderId: "781013030818",
-  appId: "1:781013030818:web:99e0a1af7e60a2946384fb",
-  measurementId: "G-2X8SL6CKRX",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 // Initialize Firebase
